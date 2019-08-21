d7102760ed7babf7e759b9c8023314a05e9845ab
dandan224 <43757729+dandan224@users.noreply.github.com>
2019-08-20 21:22:20 -0700

    Update binary_tree_path.py
    Given a binary tree, return all root-to-leaf paths from left to right.

--- binary_tree_path.py
+++ binary_tree_path.py
@@ -1,1 +1,25 @@
+Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+  def binaryTreePaths(self, root):
+    """
+    input: TreeNode root
+    return: string[]
+    """
+    # write your solution here
+    res = []
+    if not root:
+        return res
+    if not root.left and not root.right:
+        res.append(str(root.val))
+        return res
+    for path in self.binaryTreePaths(root.left):
+        res.append(str(root.val) + '->' + path)
+    for path in self.binaryTreePaths(root.right):
+        res.append(str(root.val) + '->' + path)
+    return res
 
