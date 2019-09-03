3593b9a0c8fadf4960e00ff356ab7980b8619dc0
dandan224 <43757729+dandan224@users.noreply.github.com>
2019-09-03 13:19:30 -0700

    Update Is_the_binary_search_tree_or_not.py

--- Is_the_binary_search_tree_or_not.py
+++ Is_the_binary_search_tree_or_not.py
@@ -1,3 +1,4 @@
+# solution #1:
 Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -29,3 +30,45 @@ class Solution(object):
       return (False, None, None)
     # return(True, the minimum of the root, the maximum of the root)
     return (True, left_res[1] or root.val, right_res[2] or root.val)
+  
+  # Solution#2:
+  Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+  def isBST(self, root):
+    """
+    input: TreeNode root
+    return: boolean
+    """
+    # write your solution here
+    if not root:
+      return True
+    min_val = float('-inf')
+    max_val = float('inf')
+    return self.BST(root, min_val, max_val)
+    
+  def BST(self, root, min_val, max_val):
+    if not root:
+      return True
+    if root.val <= min_val or root.val >= max_val:
+      return False
+    return self.BST(root.left, min_val, root.val) and self.BST(root.right, root.val, max_val)
+  
+  # Solution #3:by using inorder tranversal
+  def isValidBST(root):
+    prev = []
+    res = [True]
+    inorder(root, prev, res)
+    return res[0]
+  def inorder(root, prev, res):
+    if not root:
+      return
+    inorder(root.left,prev,res)
+    if prev[0] and prev[0] >= root.val:
+      res[0] = False
+    prev[0] = root.val
+    inorder(root.right, prev, res)
