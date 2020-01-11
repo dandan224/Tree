29c01a73d4eacb44e6e4c0aa4edf7fcb3a1b319a
dandan224 <43757729+dandan224@users.noreply.github.com>
2020-01-11 13:51:12 -0800

    Update Height_of_Binary_Tree.py

--- Height_of_Binary_Tree.py
+++ Height_of_Binary_Tree.py
@@ -16,3 +16,22 @@ class Solution(object):
     left = self.findHeight(root.left)
     right = self.findHeight(root.right)
     return 1 + max(left,right)
+  
+  
+#get_height of the tree(top_down method)
+def get_height_1(root):
+    if not root:
+        return 0
+    global result 
+    result = 0
+    helper(root,0)
+    return result
+    
+def helper(root, depth):
+    if not root:
+        global result
+        result = max(result, depth)
+        return
+    helper(root.left, depth + 1)
+    helper(root.right, depth + 1)
+    return
